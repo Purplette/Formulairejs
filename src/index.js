458486f1a5c76659d503570d526c86f097132a90
Purplette <47246743+Purplette@users.noreply.github.com>
2020-05-26 14:23:04 +0200

    rattrapage js et debut html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
-import moment from 'moment';
-import './style.css';
+//import style
+import './global';
+//import all images
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
 
-moment.locale('fr');
 
-const element = document.createElement('p');
-element.innerHTML =
-    `Le cours a commencé : 
-${moment("2020 04 27, 8:00:00 am", "YYYY MM DD, h:mm:ss a")
-        .fromNow()}`;
-document.body.appendChild(element);
+
+/// custom
+const username = 'user';
+const password = 'password';
+
+
+const onLoginFormSubmit = e => {
+    e.preventDefault(); // Annule l'action par défaut
+
+    setTimeout(() => {
+        // Récupère les données du formulaire
+        const data = new FormData(e.target);
+        const response = processDataForm(data);
+    }, 1000); // 1 seconde
+}
+
+//verif login
+const processDataForm = data => {
+    if (data.get('password') !== password || data.get('username') !== username) {
+        console.log("False")
+    }
+    else {
+        console.log("Success");
+    }
+}
+
+//view password
+const changePasswordView = obj => {
+   let type = obj.nextElementSibling.getAttribute("type");
+   switch (type) {
+       case "password" :
+           obj.nextElementSibling.setAttribute("type", "text");
+           break;
+       case "text" :
+           obj.nextElementSibling.setAttribute("type", "password");
+           break;
+   }
+}
+
+///////////////////////
+//events événements
+
+//submit
+document.querySelector('#loginForm').addEventListener('submit',onLoginFormSubmit);
+
+//view password
+document.querySelectorAll('.view-button').forEach((obj)=>{
+    obj.addEventListener('mousedown',()=>{
+        changePasswordView(obj);
+    });
+    obj.addEventListener('mouseup',()=>{
+        changePasswordView(obj);
+    });
+});
