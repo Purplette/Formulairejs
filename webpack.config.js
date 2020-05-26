32d97dd1109fa753ae710eb787d32a3751d9d8a4
Purplette <47246743+Purplette@users.noreply.github.com>
2020-05-26 14:33:54 +0200

    probleme affichage site : fichier blotte

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
+        publicPath: '/',
         filename: '[name].[contentHash].js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [
+                test: /\.jpe?g$|\.gif$|\.png$|\.PNG$|\.svg$|\.woff(2)?$|\.ttf$|\.eot$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'img/'
+                }
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                loader: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
-                ],
-            },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            }
         ],
+    },
+    resolve: {
+        extensions: ['.js','.scss']
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -27,9 +42,12 @@ module.exports = {
             inject: true,
             filename: 'index.html'
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin({
+          filename: '[name].[hash].css',
+          chunkFilename: '[id].[hash].css'
+        })
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        contentBase: path.join(__dirname, 'dist')
     }
 };
