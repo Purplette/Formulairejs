2cba2f64ec97c11d818b07ce5db12bfb2d9e3351
Purplette <47246743+Purplette@users.noreply.github.com>
2020-05-29 15:18:18 +0200

    rajout js pour changement des parties et rajout des pattern sur les input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //import style
 import './global';
+
 //import all images
 function importAll(r) {
     let images = {};
@@ -8,59 +9,101 @@ function importAll(r) {
 }
 const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
 
+//Fonctionnement page Inscription
+
+    //apparation des parties
+    const suivant12 = document.querySelector(".suivant12")
+    const retour21 = document.querySelector(".retour21")
+    const suivant23 = document.querySelector(".suivant23")
+    const retour32 = document.querySelector(".retour32")
+    const suivant3fin = document.querySelector(".suivant3fin")
+
+    // Button listener etape 1
+    suivant12.addEventListener("click", (e)=> {
+        document.querySelector('#InscriptionForm1').classList.remove("form-active");
+        document.querySelector('#InscriptionForm2').classList.add("form-active");
+        e.preventDefault()
+    });
+
+    // Button listener etape 2
+    retour21.addEventListener("click", (e)=> {
+        document.querySelector('#InscriptionForm2').classList.remove("form-active");
+        document.querySelector('#InscriptionForm1').classList.add("form-active");
+        e.preventDefault()
+    });
+
+    suivant23.addEventListener("click", (e)=> {
+        document.querySelector('#InscriptionForm2').classList.remove("form-active");
+        document.querySelector('#InscriptionForm3').classList.add("form-active");
+        e.preventDefault()
+    });
+
+    // Button listener etape 2
+    retour32.addEventListener("click", (e)=> {
+        document.querySelector('#InscriptionForm3').classList.remove("form-active");
+        document.querySelector('#InscriptionForm2').classList.add("form-active");
+        e.preventDefault()
+    });
+
+
+
+   
 
 
 /// custom
-const email = 'email';
-const password = 'password';
+// const email = 'email';
+// const password = 'password';
 
-
-const onLoginFormSubmit = e => {
-    e.preventDefault(); // Annule l'action par défaut
-
-    setTimeout(() => {
-        // Récupère les données du formulaire
-        const data = new FormData(e.target);
-        const response = processDataForm(data);
-    }, 1000); // 1 seconde
-}
 
 //verif login
-const processDataForm = data => {
-    if (data.get('password') !== password || data.get('email') !== email) {
-        console.log("False")
-    }
-    else {
-        console.log("Success");
+// const processDataForm = data => {
+//     if (data.get('password') !== password || data.get('email') !== email) {
+//         console.log("False")
+//     }
+//     else {
+//         console.log("Success");
 
-    }
-}
+//     }
+// }
 
 //view password
-const changePasswordView = obj => {
-   let type = obj.nextElementSibling.getAttribute("type");
-   switch (type) {
-       case "password" :
-           obj.nextElementSibling.setAttribute("type", "text");
-           break;
-       case "text" :
-           obj.nextElementSibling.setAttribute("type", "password");
-           break;
-   }
-}
 
 ///////////////////////
 //events événements
 
 //submit
-document.querySelector('#loginForm').addEventListener('submit',onLoginFormSubmit);
+// document.querySelector('#loginForm').addEventListener('submit',onLoginFormSubmit);
+
+// const onLoginFormSubmit = e => {
+//     e.preventDefault(); // Annule l'action par défaut
+
+//     setTimeout(() => {
+//         // Récupère les données du formulaire
+//         const data = new FormData(e.target);
+//         const response = processDataForm(data);
+//     }, 1000); // 1 seconde
+// }
+
 
 //view password
-document.querySelectorAll('.view-button').forEach((obj)=>{
-    obj.addEventListener('mousedown',()=>{
-        changePasswordView(obj);
-    });
-    obj.addEventListener('mouseup',()=>{
-        changePasswordView(obj);
-    });
-});
+// document.querySelectorAll('.view-button').forEach((obj)=>{
+//     obj.addEventListener('mousedown',()=>{
+//         changePasswordView(obj);
+//     });
+//     obj.addEventListener('mouseup',()=>{
+//         changePasswordView(obj);
+//     });
+// });
+
+// const changePasswordView = obj => {
+//     let type = obj.previousElementSibling.getAttribute("type");
+//     switch (type) {
+//         case "password" :
+//             obj.previousElementSibling.setAttribute("type", "text");
+//             break;
+//         case "text" :
+//             obj.previousElementSibling.setAttribute("type", "password");
+//             break;
+//     }
+//  }
+ 
