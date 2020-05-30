ff9f6f1bd7ef324ff1c8be008b49ce358ead75ae
Purplette <47246743+Purplette@users.noreply.github.com>
2020-05-30 15:20:11 +0200

    rajout de commentaires js

--- src/index.js
+++ src/index.js
@@ -8,25 +8,28 @@ function importAll(r) {
     return images;
 }
 
-const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
+//Déclaration des constantes
+    //Générales
+    const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
 
-const email = 'eva.haberthur@gmail.com';
-const password = '0000';
-const loadtime =1000;
+    //Verification informations de connexion
+    const email = 'eva.haberthur@gmail.com';
+    const password = '0000';
+    const loadtime =1000;
 
-const suivant12 = document.querySelector(".suivant12")
-const retour21 = document.querySelector(".retour21")
-const suivant23 = document.querySelector(".suivant23")
-const retour32 = document.querySelector(".retour32")
-const suivant34 = document.querySelector(".suivant34")
-const retour43 = document.querySelector(".retour43")
+    //Fonctionnement des parties de la page inscription
+    const suivant12 = document.querySelector(".suivant12")
+    const retour21 = document.querySelector(".retour21")
+    const suivant23 = document.querySelector(".suivant23")
+    const retour32 = document.querySelector(".retour32")
+    const suivant34 = document.querySelector(".suivant34")
+    const retour43 = document.querySelector(".retour43")
 
 
-
+//Fonctions
     //Fonctionnement page de Connexion
 
         //Verifications login
-
         const onFormSubmit = e => {
             e.preventDefault(); // Annule l'action par défaut
         
@@ -39,11 +42,9 @@ const retour43 = document.querySelector(".retour43")
             }, loadtime); // 1 seconde
         }
         
-
+        //Traitement des données des formulaires et applications spécifiques
         const processDataForm = (data, id) => {
 
-            //traitement indépendant des champs
-           
             switch (id) {
                 case 'loginForm':
                     if (data.get('password') !== password || data.get('email') !== email) {
@@ -68,20 +69,14 @@ const retour43 = document.querySelector(".retour43")
 
                     case'InscriptionForm3':
 
-                    document.querySelector('#InscriptionForm3').classList.remove("form-active");
-                    document.querySelector('#InscriptionForm4').classList.add("form-active");
+                        document.querySelector('#InscriptionForm3').classList.remove("form-active");
+                        document.querySelector('#InscriptionForm4').classList.add("form-active");
 
                     break;
-                    
-                        
-                        
-
             }
-
-        
         }
 
-    
+        
         //Afficher mot de passe
             const changePasswordView = obj => {
                 let el = obj.previousElementSibling;
@@ -97,7 +92,9 @@ const retour43 = document.querySelector(".retour43")
             }
         
 
-        
+//Event Listener
+            //Voir mot de passe
+
             document.querySelectorAll('.view-button').forEach((obj)=>{
                 obj.addEventListener('mousedown',()=>{
                     changePasswordView(obj);
@@ -114,30 +111,28 @@ const retour43 = document.querySelector(".retour43")
 
             });
 
-
+            //Event sur tous les submit
             document.querySelectorAll('form').forEach((obj)=>{
                 obj.addEventListener('submit',onFormSubmit);
             });
 
-    //Fonctionnement page Inscription
+    //Fonctionnement bouton retour page inscription
 
-
-    //Apparation des parties
-
-    // Button listener etape 2
+    // Retour etape 2 à 1
     retour21.addEventListener("click", (e)=> {
         document.querySelector('#InscriptionForm2').classList.remove("form-active");
         document.querySelector('#InscriptionForm1').classList.add("form-active");
         e.preventDefault()
     });
-    // Button listener etape 3    
+
+    // Retour etape 3 à 2   
     retour32.addEventListener("click", (e)=> {
         document.querySelector('#InscriptionForm3').classList.remove("form-active");
         document.querySelector('#InscriptionForm2').classList.add("form-active");
         e.preventDefault()
     });
 
-    //button listener etape 4
+    // Retour etape 4 à 3
     retour43.addEventListener("click", (e)=> {
         document.querySelector('#InscriptionForm4').classList.remove("form-active");
         document.querySelector('#InscriptionForm3').classList.add("form-active");
